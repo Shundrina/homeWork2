0bd2869ccd901065c690d7b70c166f0e61c23b02
Dariia <lystopadina@gmail.com>
2020-11-25 10:47:46 +0200

    rename the string

--- task2_1.py
+++ task2_1.py
@@ -22,7 +22,7 @@ def decrypt():
     string = request.args.get('string', '')
     result = f.decrypt(string.encode())
     res_string = result.decode()
-    return render_template('index.html', string='Encrypted string:', value=res_string)
+    return render_template('index.html', string='Decrypted string:', value=res_string)
 
 
 app.run(debug=True)
